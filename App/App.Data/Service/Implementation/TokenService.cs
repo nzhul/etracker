f0b0df33280af21e727e6f4b72bce380a9067625
Dobromir Ivanov <dobromirivanov1@gmail.com>
2017-08-01 14:21:19 +0300

    final changes

--- App/App.Data/Service/Implementation/TokenService.cs
+++ App/App.Data/Service/Implementation/TokenService.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Linq;
 using System;
 using Newtonsoft.Json;
+using System.Web;
 
 namespace App.Data.Service.Implementation
 {
@@ -21,8 +22,16 @@ namespace App.Data.Service.Implementation
 
 		public string AquireToken()
 		{
-			TokenRequest request = new TokenRequest(HttpMethod.Get, "/api/clients/subscribe?date=2016-03-10&callback=http://dev.etracker.com/api/reports");
+			string currentDomain = this.GetCurrentDomain();
+			string subscribeEndpoint = "/api/clients/subscribe?date=2016-03-10&callback=" + currentDomain + "/api/reports";
+			TokenRequest request = new TokenRequest(HttpMethod.Get, subscribeEndpoint);
 			return this.executor.ExecuteRequest(request);
+		}
+
+		private string GetCurrentDomain()
+		{
+			return HttpContext.Current.Request.Url.Scheme + System.Uri.SchemeDelimiter + HttpContext.Current.Request.Url.Host +
+			(HttpContext.Current.Request.Url.IsDefaultPort ? "" : ":" + HttpContext.Current.Request.Url.Port);
 		}
 
 		public bool SaveToken(string tokenJson)
---
+++ App/App.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace App.Web.Controllers
+{
+	public class EmployeesController : Controller
+	{
+		public ActionResult Details(int id)
+		{
+			ViewBag.EmployeeId = id;
+			return this.View();
+		}
+	}
+}
